afb878fef9cd93021c5d36f3c6b94fc509d38fff
johnny Huang <johnny8947276@gmail.com>
2023-01-12 16:26:07 +0800

    網頁模板 index ( index / gallery ) 連結

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use TCG\Voyager\Facades\Voyager;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,14 +21,23 @@ Route::get('/', function () {
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
-    'verified'
+    'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
 });
 
-
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
+});
+
+Route::namespace ('App\Http\Controllers')->group(function () {
+    Route::get('index', 'SiteController@index');
+    Route::get('gallery', 'SiteController@gallery');
+    Route::get('blog', 'SiteController@blog');
+    Route::get('blog/{blog}', 'SiteController@blog_single');
+    Route::get('contact', 'SiteController@contact');
+    Route::get('about', 'SiteController@about');
+
 });
