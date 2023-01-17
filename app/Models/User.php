127d6fba3dffaeeeb24da87f56f903ace7922295
Lin_Yu_Fan <bd1005043@gmail.com>
2023-01-17 13:07:08 +0800

    blog_single 資料匯入，加入圖片路徑

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends \TCG\Voyager\Models\User
+
 {
     use HasApiTokens;
     use HasFactory;
